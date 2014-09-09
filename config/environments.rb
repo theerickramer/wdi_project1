0be8b4db0162d9bf239222e26ed5124c52249b70
theerickramer <theerickramer@gmail.com>
2014-09-09 12:57:56 -0400

    fixed environments database

--- config/environments.rb
+++ config/environments.rb
@@ -1,14 +1,13 @@
-configure :production, :development do |
+configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-ActiveRecord::Base.establish_connection({
-	adapter: db.scheme == 'postgres' ? 'postgres' : db.scheme,
-	host: db.host,
-	username: db.user,
-	password: db.password,
-	database: db.path[1..-1],
-	encoding: 'utf8'
-	})
+	ActiveRecord::Base.establish_connection(
+			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+			:host     => db.host,
+			:username => db.user,
+			:password => db.password,
+			:database => db.path[1..-1],
+			:encoding => 'utf8'
+	)
 end
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
